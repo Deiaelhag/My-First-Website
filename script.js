de5ddfa3c001c3f932740fc4a22c69faf5180486
Deiaelhag <deiaaelhag2002@gmail.com>
2026-05-20 22:53:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function showMessage(){
-    alert("Hello Evreone!!!");
+    alert("Welcome to My Website!!!");
+}
+function showWelcomeMessage() {
+    alert("Thank you!! Your message has been sent successfully");
 }
 function changeTitle1() {
     document.getElementById("text").innerHTML = "The title has change";
 }
 function changeTitle2() {
-    document.getElementById("text").innerHTML = "Welcome to my Website!";
+    document.getElementById("text").innerHTML = "My First Website";
 }
 function changeColor() {
     document.body.style.backgroundColor = "lightgreen";
@@ -14,6 +17,7 @@ function darkMood() {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
     document.getElementById("text").style.color = "white";
+    document.querySelector("form").style.color = "black";
 }
 function hideText() {
     document.getElementById("demo").style.display = "none";
