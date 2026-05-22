1600bad80a1c3e080eefae1d27ec0aa1f7ad2034
Deiaelhag <deiaaelhag2002@gmail.com>
2026-05-22 20:40:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-function showMessage(){
+//Show success message
+function showWelcomeMessage() {
     alert("Welcome to My Website!!!");
 }
-function showWelcomeMessage() {
+function showMessage() {
     alert("Thank you!! Your message has been sent successfully");
 }
-function changeTitle1() {
+function changeTitle() {
     document.getElementById("text").innerHTML = "The title has change";
 }
-function changeTitle2() {
+function unChangeTitle() {
     document.getElementById("text").innerHTML = "My First Website";
 }
+//Change background color
 function changeColor() {
     document.body.style.backgroundColor = "lightgreen";
 }
+//Enable dark moode
 function darkMood() {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
     document.getElementById("text").style.color = "white";
-    document.querySelector("form").style.color = "black";
+    document.querySelector("form").style.backgroundColor = "black";
+    document.querySelector("form").style.color = "white";
 }
 function hideText() {
     document.getElementById("demo").style.display = "none";
